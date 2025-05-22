7a000254bfa8278418bf525e47b111a34dc9aba9
joshualin14 <23233@burnside.school.nz>
2025-05-23 09:16:31 +1200

    added first query

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-import sqllite3 
+import sqlite3 
 
 DATABASE = 'Hoi4.db'
 
+#Asks for what country you are looking for#
 def print_all_countries():
-    searching = input('What country ? ')
-with sqllite3.connect(DATABASE) as db:
-    cursor = db.cursor()
-    sql = "Select country_name, leader FROM countries"
-    cursor.execute(sql,(searching,))
+    searching = input('what country are you looking for ? ')
+    with sqlite3.connect(DATABASE) as db:
+        cursor = db.cursor()
+        sql = "Select country_name, leader FROM countries"
+        cursor.execute(sql,(searching,))
+        results = cursor.fetchall()
+
+
+
