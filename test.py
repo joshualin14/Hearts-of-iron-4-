568bb9719f04b9bf7074d565fc7bd4c9dea64d17
joshualin14 <23233@burnside.school.nz>
2025-05-26 10:47:58 +1200

    added  title screen to the db

--- test.py
+++ test.py
@@ -1,21 +1,42 @@
 import sqlite3 
 
 DATABASE = 'Hoi4.db'
+while True:
+    print("What are you looking for ?")
+
+    print("print all countires = 1")
+    print("print by leader = 2")
+    print("print by date = 3") 
+    print("print by ideology = 4")
+    print("Exit the database = 5")
+    Promt = input("Searching for ? ")
+    
+   
+
 
 #Asks for what country you are looking for#
-def print_all_countries():
-    searching = input('what country are you looking for ? ')
+def country():
     with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
-        sql = "Select country_name, leader FROM countries"
-        cursor.execute(sql,(searching,))
-        results = cursor.fetchall()
+            cursor = db.cursor()
+            sql = "Select country_name, leader FROM countries"
+            cursor.execute(sql,(searching,))
+            results = cursor.fetchall()
 
 
-        for country in results:
-            print(f"Country: {country[2]} Leader : {country[3]}")
+            for country in results:
+                 print(f"Country: {country[2]} ")
+db.close()
+                
 
-if __name__ == "__main__":
-    print_all_countries()
+
+   
+
+
+
+
+  
+
+
+
 
 
